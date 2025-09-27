1595ff276c6f0f9194c439424dd963705d31818f
Annarita Pascazio <128156806+AnnaritaPascazio@users.noreply.github.com>
2025-09-27 19:19:05 +0200

    fixing_bugs

--- project_1/script.js
+++ project_1/script.js
@@ -1,11 +1,17 @@
 startTimer(5,"timer");
 
 function startTimer(secs,elem){
-    let t = document.getElementById(elem); // call this every single time the function is called - every second
-    t.innerHTML = "<h1>00:" + secs + "</h1>";
+    let t = document.getElementById(elem);
+     // call this every single time the function is called - every second
+     if(secs.toString().lenght == 1){
+        t.innerHTML = "<h1>00: " + secs +  "</h1>";
+     }else{
+          t.innerHTML = "<h1>00:" + secs + "</h1>";
+     }
+  
     if(secs < 1){
-        clearTimeout(countDown);
         t.innerHTML = "<h1>00:00</h1>"
+        clearTimeout(countDown);
     }
     secs--;
     let countDown = 
